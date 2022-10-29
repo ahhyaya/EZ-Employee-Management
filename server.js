78c54fb861da45fd1ce304402b58e7c87b0e971d
doldol123 <toruig59@gmail.com>
2022-10-28 21:03:54 -0700

    add connection to database

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'organization_db'
+    },
+    console.log(`Successfully connected to the organization_db database!`)
+);
