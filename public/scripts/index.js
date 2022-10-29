efce61a7063314366066486f77b51f4e5bf398c5
doldol123 <toruig59@gmail.com>
2022-10-29 10:40:06 -0700

    add mainquestions and choices

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,21 @@
 const inquirer = require('inquirer');
+const fs = require ('fs');
+
+
+const mainQuestions = [
+    {
+        type: 'list',
+        name: 'actions',
+        message: 'What would you like to do?'
+        choices: [
+            'View All Departments',
+            'Add Department',
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role'
+        ]
+    },
+   
+]
